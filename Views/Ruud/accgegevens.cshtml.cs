fcfcca3c4a2d27632bc4900b68842028f81f98c9
gelebus <61007922+gelebus@users.noreply.github.com>
2020-03-19 14:41:57 +0100

    First commit
    .

---
+++ Views/Ruud/accgegevens.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Dashboard.Pages
+{
+    public class accgegevensModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public accgegevensModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
