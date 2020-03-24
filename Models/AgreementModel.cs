a506daa5cbb71ab7791db21431ce105758169ea3
Coding-is-an-adventure <51972581+Coding-is-an-adventure@users.noreply.github.com>
2020-03-24 15:32:49 +0100

    24-3-2020 Verwerking Behandelsovereenkomst
    Connectie Database en AgreementModel

---
+++ Models/AgreementModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace EurocomV2.Models
+{
+    public class AgreementModel
+    {
+        private SqlConnection dataBaseConnection;
+        private AgreementModel agreementModel;
+
+        public AgreementModel()
+        {
+            agreementModel = new AgreementModel();
+        }
+
+        public bool AgreementAccepted { get; private set; }
+
+        public bool GetAgreementStatus()
+        {
+            agreementModel.DataBaseCommand("GetAgreement");
+            return true;
+        }
+
+        public void UpdateAgreementStatus(bool agreed)
+        {
+            agreementModel.DataBaseCommand("UpdateAgreement");
+        }
+        
+        //Helper Methods 
+        private void DataBaseConnection()
+        {
+            dataBaseConnection.Open();
+
+            dataBaseConnection.Close();
+        }
+
+        private void DataBaseCommand(string storedProcedureName)
+        {
+            SqlCommand cmd = new SqlCommand(storedProcedureName, dataBaseConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+        }
+    }
+}
