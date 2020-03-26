c73d2652596217789e31e73b9e0c170df9daec94
Julian <Julian-Tekstra@hotmail.com>
2020-03-26 12:37:43 +0100

    Changes in database and code to clean up data

--- Controllers/StatusController.cs
+++ Controllers/StatusController.cs
@@ -15,21 +15,40 @@ namespace EurocomV2.Controllers
         SqlConnection sqlConnection = new SqlConnection("server = (LocalDB)\\MSSQLLocalDB; database = EurocomJulian; Trusted_Connection = true; MultipleActiveResultSets = True");
         public IActionResult Status()
         {
-
+            var selectedUser = TempData["SelectedUser"];
 
 
             Random rnd = new Random();
             double inrValue = rnd.NextDouble(1.0, 5.0);
 
             inrValue = Math.Round(inrValue, 1);
+            SqlCommand checkAmount = new SqlCommand("CheckAmount", sqlConnection);
+            checkAmount.CommandType = CommandType.StoredProcedure;
+            SqlParameter amount = new SqlParameter();
+            checkAmount.Parameters.AddWithValue("@userID", selectedUser);
+            amount.ParameterName = "@amount";
+            amount.SqlDbType = SqlDbType.Int;
+            amount.Direction = ParameterDirection.Output;
+            checkAmount.Parameters.Add(amount);
+            sqlConnection.Open();
+            checkAmount.ExecuteNonQuery();
+            sqlConnection.Close();
+            int statusAmount = Convert.ToInt32(amount.Value);
+            if (statusAmount > 10)
+            {
+                SqlCommand selectOldest = new SqlCommand("DELETE FROM PatientStatus WHERE lowest = (SELECT MIN(statusID) from PatientStatus WHERE @userID = userID)");
+                selectOldest.Parameters.AddWithValue("@userID", selectedUser);
+                sqlConnection.Open();
+                sqlConnection.Close();
+            }
 
-            SqlCommand updateStatus = new SqlCommand("UPDATE PatientStatus SET IR = @IR WHERE @userID = userID", sqlConnection);
+            SqlCommand updateStatus = new SqlCommand("INSERT INTO  PatientStatus(userID, IR) VALUES(@userID, @IR)", sqlConnection);
             updateStatus.CommandType = CommandType.Text;
-            updateStatus.Parameters.AddWithValue("@userID", TempData["SelectedUser"]);
+            updateStatus.Parameters.AddWithValue("@userID", selectedUser);
             updateStatus.Parameters.AddWithValue("@IR", inrValue);
             SqlCommand checkStatus = new SqlCommand("CheckStatus", sqlConnection);
             checkStatus.CommandType = CommandType.StoredProcedure;
-            checkStatus.Parameters.AddWithValue("@userID", TempData["SelectedUser"]);
+            checkStatus.Parameters.AddWithValue("@userID", selectedUser);
             sqlConnection.Open();
             updateStatus.ExecuteNonQuery();
             checkStatus.ExecuteNonQuery();
