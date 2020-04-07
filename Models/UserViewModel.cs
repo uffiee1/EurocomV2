5021f1a7c085b1dc7f48d184622e4d4292362e2b
Zandzakje <45821233+Zandzakje@users.noreply.github.com>
2020-04-07 15:43:49 +0200

    toevoegen doktoren setup

---
+++ Models/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EurocomV2.Models
+{
+    public class UserViewModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Phonenumber { get; set; }
+        public bool Agreement { get; set; }
+    }
+}
