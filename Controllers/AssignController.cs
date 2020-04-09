bf4673175e940fe2648b8571808cc2a509677b4f
Zandzakje <45821233+Zandzakje@users.noreply.github.com>
2020-04-09 16:20:59 +0200

    verder gewerkt aan de opzet (aparte projecten en klassen per apart project)

--- Controllers/AssignController.cs
+++ Controllers/AssignController.cs
@@ -3,14 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using EurocomV2.Models;
+using EurocomV2_Model;
+using EurocomV2_Logic.Container;
 
 namespace EurocomV2.Controllers
 {
     public class AssignController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AssignStart()
+        {
+            AssignViewModel assignViewModel = new AssignViewModel
+            {
+                patients = GetPatientNames()
+            };
+            return View("Index", assignViewModel);
+        }
+
+        public ActionResult AssignClick(string firstname, string lastname)
+        {
+            DoctorContainer doctorContainer = new DoctorContainer();
+            string username = "dTest2";
+            doctorContainer.UseAddPatientToDoctor(username, firstname, lastname);
+            AssignViewModel assignViewModel = new AssignViewModel
+            {
+                patients = GetPatientNames(),
+                assignSuccess = true,
+                assignMessage = "Patient successfully added to doctor!"
+            };
+
+            return View("Index", assignViewModel);
+        }
+
+        public List<PatientViewModel> GetPatientNames()
+        {
+            PatientContainer patientContainer = new PatientContainer();
+            List<PatientModel> patientsModel = patientContainer.RetreivePatientNames();
+            List<PatientViewModel> patients = new List<PatientViewModel>();
+            foreach(PatientModel patient in patientsModel)
+            {
+                PatientViewModel patientViewModel = new PatientViewModel
+                {
+                    Firstname = patient.Firstname,
+                    Lastname = patient.Lastname
+                };
+                patients.Add(patientViewModel);
+            }
+            return patients;
         }
     }
 }
--- Models/AssignViewModel.cs
+++ Models/AssignViewModel.cs
@@ -7,5 +7,9 @@ namespace EurocomV2.Models
 {
     public class AssignViewModel
     {
+        public DoctorViewModel doctorViewModel { get; set; }
+        public List<PatientViewModel> patients { get; set; }
+        public bool assignSuccess { get; set; }
+        public string assignMessage { get; set; }
     }
 }
