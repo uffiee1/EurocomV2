a39b572ba9001ea4ce8ef1d32bc0e37e37823748
gelebus <61007922+gelebus@users.noreply.github.com>
2020-04-09 15:36:33 +0200

    Werkende gegevenspagina
    Ik heb alle gegevens van doctors/patienten in 2 klassen in listen gezet. Ik heb hier ook al dat het werkt met meerdere doctors per patient.

--- Views/connectionstring.cs
+++ Views/connectionstring.cs
@@ -4,24 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using EurocomV2.Views;
 
 namespace EurocomV2
 {
     public class connectionstring
     {
-        public List<string> FirstName = new List<string>();
-        public List<string> LastName = new List<string>();
-        public List<string> Number = new List<string>();
-        public List<string> Doctor = new List<string>();
-        public List<string> NumberDoctor = new List<string>();
+        public List<patient> patients = new List<patient>();
+        public List<Doctor> Doctors = new List<Doctor>();
+        public List<Doctor> DoctorsPerPatient = new List<Doctor>();
 
-        public void Main()
+        public void Main(int idMinEen)
         {
             string str = "Data Source = mssql.fhict.local; Initial Catalog = dbi406383_eurocom; Persist Security Info = True; User ID = dbi406383_eurocom; Password = Kastanje81;";
-            string str2 = "SELECT Firstname, Lastname, PhoneNumber FROM [User] WHERE Username LIKE 'p%';";
+            string str2 = "SELECT Firstname, Lastname, PhoneNumber, Username FROM [User] WHERE Username LIKE 'p%';";
             string str3 = "SELECT Firstname, PhoneNumber FROM [User] WHERE Username LIKE 'd%';";
             ReadData(str, str2);
             ReadData2(str, str3);
+            ReadData3(str, idMinEen);
 
         }
         
@@ -37,13 +37,13 @@ namespace EurocomV2
                 // Call Read before accessing data.
                 while (reader.Read())
                 {
+                    
                     string LastName2 = reader.GetString("Lastname");
                     string FirstName2 = reader.GetString("Firstname");
                     string number = reader.GetString("PhoneNumber");
-                    
-                    FirstName.Add(FirstName2);
-                    LastName.Add(LastName2);
-                    Number.Add(number);
+                    string UserName = reader.GetString("Username");
+                    patient Patient = new patient(FirstName2, LastName2, number, UserName);
+                    patients.Add(Patient);
                 }
 
                 // Call Close when done reading.
@@ -63,13 +63,40 @@ namespace EurocomV2
                 // Call Read before accessing data.
                 while (reader.Read())
                 {
+                    string doctor = reader.GetString("Firstname");
+                    string numberdoctor = reader.GetString("PhoneNumber");
                     
-                      string doctor = reader.GetString("Firstname");
-                      string numberdoctor = reader.GetString("PhoneNumber");
-                      
-                    
-                      Doctor.Add(doctor);
-                      NumberDoctor.Add(numberdoctor);
+
+                    Doctor Doctor1 = new Doctor(doctor, numberdoctor);
+                    Doctors.Add(Doctor1);  
+                }
+
+                // Call Close when done reading.
+                reader.Close();
+            }
+        }
+        private void ReadData3(string connectionString, int idMinEen)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("PatientOverview", connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@UserName", patients[idMinEen].UserName));
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+
+                    string Name = reader.GetString(3);
+                    string Number = reader.GetString(4);
+
+                    Doctor doctor = new Doctor(Name, Number);
+                    DoctorsPerPatient.Add(doctor);
                 }
 
                 // Call Close when done reading.
---
+++ Views/patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EurocomV2.Views
+{
+    public class patient
+    {
+        public patient(string firstName, string lastName, string number, string username)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Number = number;
+            UserName = username;
+        }
+
+        public string FirstName { get; private set;}
+        public string LastName { get; private set; }
+        public string Number { get; private set; }
+        public string UserName { get; private set; }
+    }
+}
