c541c995a41902511ea8a04585d885ad38f5a3ac
Ufuk <u.angay@gmail.com>
2020-05-19 15:50:15 +0200

    Added Role Class-Layout-WIP

---
+++ Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EurocomV2.Models
+{
+    public class Role
+    {
+        public const string Administrator = "Admin";
+        public const string Doctor = "Dokter";
+        public const string User = "Patient";
+    }
+}
