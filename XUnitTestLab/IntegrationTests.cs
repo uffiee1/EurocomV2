02506c9375dbad1242f5a60b6e1ba94d1f1c2694
Coding-is-an-adventure <51972581+Coding-is-an-adventure@users.noreply.github.com>
2020-05-26 13:53:57 +0200

    Seperated Layers
    26-05-2020
    Added:
    - Interface Layer
    - Data Layer
    - Logic Layer

---
+++ XUnitTestLab/IntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestLab
+{
+    public class IntegrationTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ XUnitTestLab/UnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestLab
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
