75b07433dc536f842bca5be6b4f3eaf69e62b834
Julian <Julian-Tekstra@hotmail.com>
2020-06-01 10:16:34 +0200

    Changed structure more

---
+++ Data_Layer/RootObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Layer
+{
+   public class RootObject
+    {
+        public List<MeasurementDTO> MeasurementDtos { get; set; }
+    }
+}
