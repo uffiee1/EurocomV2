867e0e0f0eb74f5a3aef654fb0be5819758c493e
Zandzakje <45821233+Zandzakje@users.noreply.github.com>
2020-06-09 14:52:22 +0200

    Project na de aanpassing structuur
    :D

---
+++ EurocomV2_Data/DoctorData.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using EurocomV2_Data.DTO;
+
+namespace EurocomV2_Data
+{
+    public class DoctorData
+    {
+        public void AddPatientToDoctor(string username, int userId)
+        {
+            using (ConnectionString connectionString = new ConnectionString())
+            {
+                try
+                {
+                    connectionString.sqlConnection.Open();
+                    SqlCommand cmd = new SqlCommand("AssignPatientToDoctor", connectionString.sqlConnection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception exception)
+                {
+                    if(exception is InvalidCastException || exception is SqlException)
+                    {
+                        Console.WriteLine("Error source: " + exception);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public AssignDTO CheckSecurityCodeMatch(string securityCode)
+        {
+            AssignDTO assignDTO = new AssignDTO();
+            using (ConnectionString connectionString = new ConnectionString())
+            {
+                try
+                {
+                    connectionString.sqlConnection.Open();
+                    SqlCommand cmd = new SqlCommand("CheckSecurityCode", connectionString.sqlConnection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@securityCode", securityCode);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            assignDTO = new AssignDTO
+                            {
+                                patientDTO = new PatientDTO { UserId = reader.GetInt32(0) },
+                                SecurityCodeMatch = true
+                            };
+                        }
+                        else
+                        {
+                            assignDTO = new AssignDTO
+                            {
+                                SecurityCodeMatch = false
+                            };
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    if (exception is InvalidCastException || exception is SqlException)
+                    {
+                        Console.WriteLine("Error source: " + exception);
+                        throw;
+                    }
+                }
+            }
+            return assignDTO;
+        }
+
+        public bool CheckExistingRelationDoctorPatient(string username, int userId)
+        {
+            AssignDTO assignDTO = new AssignDTO();
+            using (ConnectionString connectionString = new ConnectionString())
+            {
+                try
+                {
+                    connectionString.sqlConnection.Open();
+                    SqlCommand cmd = new SqlCommand("CheckExistingRelationDoctorPatient", connectionString.sqlConnection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if(reader.Read())
+                        {
+                            assignDTO.ExistingRelation = true;
+                        }
+                        else
+                        {
+                            assignDTO.ExistingRelation = false;
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    if (exception is InvalidCastException || exception is SqlException)
+                    {
+                        Console.WriteLine("Error source: " + exception);
+                        throw;
+                    }
+                }
+            }
+            return assignDTO.ExistingRelation;
+        }
+    }
+}
