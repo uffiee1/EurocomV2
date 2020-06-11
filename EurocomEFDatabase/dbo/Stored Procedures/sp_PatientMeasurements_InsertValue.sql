436b509ce61a6787534db7b6c9d9fd2bd1decd62
Tony <tony.zhou125@gmail.com>
2020-06-11 15:49:38 +0200

    Create Back-up Database

---
+++ EurocomEFDatabase/dbo/Stored Procedures/sp_PatientMeasurements_InsertValue.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[sp_PatientMeasurements_InsertValue]
+	@Id INT,
+	@Date DATE,
+	@Measurement INT,
+	@MeasurementSucceeded BIT 
+AS
+	IF NOT EXISTS(SELECT [Id]
+		          FROM [dbo].[PatientMeasurements] 
+			      WHERE [Id] = @Id)
+		BEGIN 
+			INSERT INTO [dbo].[PatientMeasurements] ([Id], [Date], [Measurement], [MeasurementSucceeded])
+			VALUES (@Id, @Date, @Measurement, @MeasurementSucceeded)
+		END 
+
+	ELSE 
+		BEGIN
+			RAISERROR('The measurement already exist.', 16, 1)
+		END 
+   
+RETURN 0
