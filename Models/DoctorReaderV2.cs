0a0240fcb5f438269f96ce6ed0f134a2d729acc0
gelebus <61007922+gelebus@users.noreply.github.com>
2020-06-16 13:50:34 +0200

    Aanpassingen zonder tests

---
+++ Models/DoctorReaderV2.cs
@@ -0,0 +1,88 @@
+﻿using EurocomV2.Views;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EurocomV2.Models
+{
+    public class DoctorReaderV2
+    {
+        public List<patient> patients = new List<patient>();
+        string userIds = "";
+        public string Copy = "";
+        public string ConnectionString = "Data Source = mssql.fhict.local; Initial Catalog = dbi406383_eurocomv2; Persist Security Info = True; User ID = dbi406383_eurocomV2; Password = Handjeklap1234;";
+            
+        public void Read(int UserId)
+        {
+            userIds = "";
+            patients.Clear();
+
+            string str2 = "SELECT patientId FROM [PatientDokterKoppel] WHERE doktersId = '" + UserId + "'";
+            Copy = str2;
+
+            ReadPatients(ReadPatientIds(ConnectionString, str2), ConnectionString);
+        }
+        // Reads all patientIds that are linked to the doctorId that was found in ReadDocterId
+        public List<string> ReadPatientIds(string connectionString, string queryString)
+        {
+            List<string> patientIds = new List<string>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    patientIds.Add(Convert.ToString(reader.GetInt32("patientId")));
+                }
+                // Call Close when done reading.
+                reader.Close();
+            }
+
+            return patientIds;
+        }
+        // Reads all the patients with the UserIds from ReadUserIds
+        public void ReadPatients(List<string> UserIds, string connectionString)
+        {
+            for (int i = 0; i < UserIds.Count; i++)
+            {
+                if (UserIds.Count == 1 || UserIds.Count - 1 == i)
+                {
+                    userIds += UserIds[i];
+                }
+                else
+                {
+                    userIds += UserIds[i] + ", ";
+                }
+            }
+
+            string queryString = "SELECT UserName, PhoneNumber, Email FROM [AspNetUsers] WHERE Id IN (" + userIds + ");";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    string Name = reader.GetString("UserName");
+                    string Email = reader.GetString("Email");
+                    string Number = reader.GetString("PhoneNumber");
+                    string UserName = reader.GetString("Username");
+                    patient Patient = new patient(Name, Email, Number, UserName);
+                    patients.Add(Patient);
+                }
+                // Call Close when done reading.
+                reader.Close();
+            }
+        }
+    }
+}
