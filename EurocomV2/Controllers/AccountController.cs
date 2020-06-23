b0dbf826be1469f5d53deb5190cbed83e440d5b6
Julian <Julian-Tekstra@hotmail.com>
2020-06-23 15:20:57 +0200

    Merge deel 2

--- EurocomV2/Controllers/AccountController.cs
+++ EurocomV2/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿/*using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using EurocomV2.Models;
 using EurocomV2.Models.Classes;
@@ -10,139 +12,79 @@ using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using Data_Layer;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Extensions.Logging;
+using RegisterViewModel = EurocomV2.ViewModels.RegisterViewModel;
+using LoginViewModel = EurocomV2.ViewModels.LoginViewModel;
 
 namespace EurocomV2.Controllers
 {
-
-    [Authorize(Roles = Role.Administrator)]
     public class AccountController : Controller
     {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly ILogger logger;
 
-        [BindProperty] public RegisterViewModel RegisterViewModel { get; set; }
-
-        public AccountController(UserManager<User> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager, ILogger logger)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
-            this.logger = logger;
         }
 
-        [Authorize(Roles = Role.Administrator)]
+        //Only Admin & Doctor have permission to register new user
         [HttpGet]
+        [Authorize(Roles = Role.Administrator)]
+        [Authorize(Roles = Role.Doctor)]
         public IActionResult Register()
         {
-          /* var RegisterViewModel = new RegisterViewModel
+            var model = new RegisterViewModel
             {
-                RoleItems = roleManager.Roles.Select(iR => new SelectListItem
+                RoleItems = _roleManager.Roles.Select(iR => new SelectListItem
                 {
                     Text = iR.Name,
                     Value = iR.Name
                 })
             };
-            return View(RegisterViewModel);
-        }
-
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            ApplicationUser user = new ApplicationUser() { UserName = model.FirstName + model.LastName, Name = model.FirstName + " " + model.LastName, Email = model.Email, gender = model.gender, FirstName = model.FirstName, LastName = model.LastName, PhoneNumber = model.PhoneNumber };
-            var result = await _userManager.CreateAsync(user, model.Password);
-
-            if (result.Succeeded)
-            {
-                await _signInManager.SignInAsync(user, isPersistent: false);
-                TempData["UserID"] = user.Id;
-                return RedirectToAction("Index", "Home");
-            }
-
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(String.Empty, error.Description);
-            }
             return View(model);
         }
 
-
-
-
-
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return View();
-        }
-
         [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var user = await _userManager.FindByEmailAsync(model.Email);
-
-                if (user != null)
+                var user = new ApplicationUser() 
                 {
-                    var result =
-                        await _signInManager.PasswordSignInAsync(user.Username, model.Password, model.rememberMe,
-                            false);
-                    if (result.Succeeded)
-                    {
-                        string DeviceID = ProcessAPIData.GetClient(await ProcessAPIData.GetAllDevices(), null);
-                        StatusViewModel data = new StatusViewModel()
-                        {
-                            Measurement =
-                                ProcessAPIData.GetMostRecentDate(await ProcessAPIData.GetMeasurementData(DeviceID)),
-                            InrDto = await ProcessAPIData.LoadInrData(DeviceID)
-                        };
-                        if (data != null)
-                        {
-                            TempData["Id"] = data.InrDto.id;
-                            return RedirectToAction("Status", "Home");
-                        }
-                    }
-                    ModelState.AddModelError(string.Empty, "Email/Password combination invalid");
+                UserName = model.FirstName + model.LastName, Name = model.FirstName + " " + model.LastName,
+                Email = model.Email, gender = model.gender, FirstName = model.FirstName, LastName = model.LastName,
+                PhoneNumber = model.PhoneNumber
+                };
+            
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (!await _roleManager.RoleExistsAsync(Role.User))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(Role.User));
                 }
+
+                if (!await _roleManager.RoleExistsAsync(Role.Administrator))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(Role.Administrator));
+                }
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    TempData["UserID"] = user.Id;
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(String.Empty, error.Description);
+                }
+                return View(model);
             }
-
-            return View(model);
-
-            }
-
-            public IActionResult Logout()
-            {
-                _signInManager.SignOutAsync();
-                return RedirectToAction("Login");
-            }
-        }
-    }
-*/
-using System;
-using System.Threading.Tasks;
-using EurocomV2.Models.Classes;
-using EurocomV2.ViewModels;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-
-namespace EurocomV2.Controllers
-{
-
-    public class AccountController : Controller
-    {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
+            
         [HttpGet]
         public IActionResult Register()
         {
@@ -169,52 +111,15 @@ namespace EurocomV2.Controllers
             return View(model);
         }
 
-
-
-
-
+        //Login : GET
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Login(LoginViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var user = await _userManager.FindByEmailAsync(model.Email);
-
-        //        if (user != null)
-        //        {
-        //            var result =
-        //                await _signInManager.PasswordSignInAsync(user.UserName, model.Password, model.rememberMe,
-        //                    false);
-        //            if (result.Succeeded)
-        //            {
-        //                string DeviceID = ProcessAPIData.GetClient(await ProcessAPIData.GetAllDevices(), user.Name);
-        //                StatusViewModel data = new StatusViewModel()
-        //                {
-        //                    Measurement =
-        //                        ProcessAPIData.GetMostRecentDate(await ProcessAPIData.GetMeasurementData(DeviceID)),
-        //                    InrDto = await ProcessAPIData.LoadInrData(DeviceID)
-        //                };
-        //                if (data != null)
-        //                {
-        //                    TempData["Id"] = data.InrDto.id;
-        //                    return RedirectToAction("Status", "Home");
-        //                }
-        //            }
-        //            ModelState.AddModelError(string.Empty, "Email/Password combination invalid");
-        //        }
-        //    }
-
-        //    return View(model);
-
-        //}
-
-        //Mockup voor demo
+        //Login : POST
+        //Checks whether the correct combination of the entered email address and passwords are correct
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
@@ -229,16 +134,24 @@ namespace EurocomV2.Controllers
                     {
                         return RedirectToAction("Status", "Home");
                     }
+                    ModelState.AddModelError(string.Empty, "Email en/of Wachtwoord is incorrect. Probeer het opnieuw.");
                 }
 
             }
             return View(model);
         }
 
+        // Logout button by header section
         public IActionResult Logout()
         {
             _signInManager.SignOutAsync();
             return RedirectToAction("Login");
+        }
+
+        //When user don't have permission to access some pages
+        public IActionResult AccesDenied()
+        {
+            return View();
         }
     }
 }
